4a7a772c33d4b862c22adf04003b4f4f65a078a4
Bill Cruise <cruise02@gmail.com>
2014-08-05 18:12:29 -0400

    Added method Node deleteNode(Node head, int d).
    Delete the first Node with the given value from a linked list.

--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -26,4 +26,27 @@ public class Node {
         }
         n.next = end;
     }
+    
+    /**
+     * Delete the first Node with the given value from a linked list.
+     * @param head
+     * @param d
+     * @return a linked list without the deleted Node.
+     */
+    Node deleteNode(Node head, int d) {
+        Node n = head;
+        
+        if(n.data == d) {
+            return head.next; // return a list that excludes the current head
+        }
+        
+        while(n.next != null) {
+            if(n.next.data == d) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+        return head;
+    }
 }
